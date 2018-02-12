74b13d8bbd97431500b56cb6b8b48f7b637f0dc0
Nikita Andreiev <nickandreev47@gmail.com>
2018-02-12 16:09:25 +0300

    eleventh commit

--- src/main/java/salvo/salvo/WebController.java
+++ src/main/java/salvo/salvo/WebController.java
@@ -5,10 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping(path="/", method = RequestMethod.GET)
 public class WebController {
 
-    @RequestMapping(path="/", method = RequestMethod.GET)
     public String index(){
         return "https://vast-waters-37875.herokuapp.com/web/authentication.html";
     }
+
 }
