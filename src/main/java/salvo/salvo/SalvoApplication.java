adbeacc044a4b241c125a81a7f96089fd2924047
Nikita Andreiev <nickandreev47@gmail.com>
2018-02-13 11:52:20 +0300

    fourteenth commit

--- src/main/java/salvo/salvo/SalvoApplication.java
+++ src/main/java/salvo/salvo/SalvoApplication.java
@@ -137,6 +137,7 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+				.antMatchers("/").permitAll()
 				.antMatchers("/web/authentication.html").permitAll()
 				.antMatchers("/web/style.css").permitAll()
 				.antMatchers("/web/authentication.js").permitAll()
